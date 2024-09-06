c77bff32a502ba65d2705a1778babc74481dd620
Hariiikrishnan <therihari36@gmail.com>
2024-09-06 21:50:46 +0530

    Initial

---
+++ fetchevents.php
@@ -0,0 +1,36 @@
+
+
+
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "event_calendar";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    echo json_encode([]);
+    exit();
+}
+
+$sql = "SELECT id, title, description, start_date, end_date, start_time, end_time FROM events";
+$result = $conn->query($sql);
+
+$events = [];
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $events[] = $row;
+    }
+}
+
+echo json_encode($events);
+
+$conn->close();
+?>
